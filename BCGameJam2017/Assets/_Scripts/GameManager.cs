fa6b782c361f782f2ad493f179296138b8a87205
Denis Turitsa <dturitsa@gmail.com>
2017-02-11 16:09:31 -0800

    background stuff

--- BCGameJam2017/Assets/_Scripts/GameManager.cs
+++ BCGameJam2017/Assets/_Scripts/GameManager.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    public GameObject carbonDioxide, methane, h2o, n2o;
+    public float moleculeSpawnMinX = -200, moleculeSpawnMaxX = 200, moleculeSpawnMinY = 40, moleculeSpawnMaxY = 100;
+    public float startTemp = 20;
+    public float maxTemp = 100;
+    public float tempIncreaseRate = 10;
+    public float tempDecreaseRate = 5;
+    public float WaterMovementRange = 100, iceMovementRange = 200;
+    public float waterSpeed = 10, iceSpeed = 20;
+    public Transform water, iceberg;
+    public temperature thermometer;
+
+
     private PersistantData persistantData;
     private Vector3 randomPosition;
     private float x, y;
-    public GameObject carbonDioxide, methane, h2o, n2o;
-
-    public float moleculeSpawnMinX = -200, moleculeSpawnMaxX = 200, moleculeSpawnMinY = 40, moleculeSpawnMaxY = 100;
-
-    public float startTemp = 20;
-    public float maxTemp = 100;
-
-    public float tempIncreaseRate = 10;
-    public float tempDecreaseRate = 5;
-
     private float currentTemp;
+    private float waterStartY, iceStartY;
 
     void Start() {
         persistantData = (PersistantData)FindObjectOfType(typeof(PersistantData));
@@ -27,12 +30,21 @@ public class GameManager : MonoBehaviour {
         spawnMolecules(methane, persistantData.methaneCounter);
         spawnMolecules(h2o, persistantData.h2oCounter);
         spawnMolecules(n2o, persistantData.n2oCounter);
+
+        waterStartY = water.position.y;
+        iceStartY = iceberg.position.y;
     }
 
     void Update() {
         currentTemp += tempIncreaseRate * Time.deltaTime;
-       // Debug.Log(currentTemp);
+        // Debug.Log(currentTemp);
+        thermometer.temp = currentTemp;
     }
+    private void smoothedWaterMovement() {
+        Vector3 waterTarget = new Vector3(water.position.x, waterStartY + WaterMovementRange * currentTemp);
+      //  water.position = Vector3.MoveTowards(transform.position, target.position, waterSpeed * Time.deltaTime);
+    }
+
     public void reduceTemp() {
         currentTemp -= tempDecreaseRate;
     }
